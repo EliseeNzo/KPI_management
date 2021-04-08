1c062fc6816301c7383539b1145a928ffc7ae56c
unknown <elisee.nzorubara@gmail.com>
2021-04-08 17:17:35 +0200

    next front end test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,13 +16,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
-        <form>
-          <label>
-            Nom :
-            <input type="text" name="name" />
-          </label>
-          <input type="submit" value="Envoyer" />
-        </form>
       </header>
     </div>
   );
